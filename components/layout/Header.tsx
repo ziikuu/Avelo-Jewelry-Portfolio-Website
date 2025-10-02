cfd31aefaa79fa604cdc5a1c4cf1eec9e8189f09
ziikuu <angelo.bringula@gmail.com>
2025-10-02 20:55:43 +0800

    updated the links in the header and added /Portfolio page

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Button } from '../ui'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <header className='responsive-margin mt-6 md:mt-8 lg:mt-16 top-0 sticky flex'>
-      <h2 className='mr-auto'>Repair Shop</h2>
+      <Link href='/' className='mr-auto'>
+        <h2>Repair Shop</h2>
+      </Link>
       <Button />
     </header>
   )
--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -6,9 +6,9 @@ const Sidebar = () => {
     <aside className='sticky top-0 left-0 max-md:hidden font-black mr-5 h-full w-[20%] max-w-[202px] pt-16'>
         <nav className=''>
             <ul className=''>
-                <li><Link href='#Home'><p className=''>Home</p></Link></li>
+                <li><Link href='/'><p className=''>Home</p></Link></li>
                 <li><Link href='#About'><p className=''>About</p></Link></li>
-                <li><Link href='#Portfolio'><p className=''>Portfolio</p></Link></li>
+                <li><Link href='/Portfolio'><p className=''>Portfolio</p></Link></li>
                 <li><Link href='#Contacts'><p className=''>Contacts</p></Link></li>
             </ul>
         </nav>
