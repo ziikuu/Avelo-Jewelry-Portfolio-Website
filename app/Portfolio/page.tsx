3aca534c51f6711ba3e238802d9a35cf36dce0e6
ziikuu <angelo.bringula@gmail.com>
2025-10-22 23:50:19 +0800

    update page.tsx and ProductCard.tsx. added style for the product card

--- app/Portfolio/page.tsx
+++ app/Portfolio/page.tsx
@@ -5,21 +5,19 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex justify-center flex-col'>
-      <div className='flex justify-center mt-10 p-5 w-screen'>
-        <h2 className=''>Our Collection</h2>
-      </div>
+      <h2 className='mt-10 mb-10 mr-auto ml-auto'>Our Collection</h2>
       {/* Product cards */}
       {Object.keys(portfolioCounts).map( (category) => (
         <div key={category} className='mb-10'>
           <h3 className='capitalize'>{category}</h3>
-          <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-            {getCategoryImages(category as keyof typeof portfolioCounts).map((src, i) => (
-              <ProductCard
-                key={src}
-                src={src}
-                alt={`${category} ${i + 1}`}
-              />
-            ))}
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+              {getCategoryImages(category as keyof typeof portfolioCounts).map((src, i) => (
+                <ProductCard
+                  key={src}
+                  src={src}
+                  alt={`${category} ${i + 1}`}
+                />
+              ))}
           </div>
         </div>
       ))}
--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -8,14 +8,17 @@ type ProductCardProps = {
 
 export default function ProductCard({ src, alt }: ProductCardProps) {
   return(
-    <div>
-      <Image
-        src={src}
-        alt={alt}
-        width={300}
-        height={300} 
-      />
-      <div>{alt}</div>
+    <div className='flex flex-col justify-center items-center overflow-hidden bg-background rounded-xl'>
+      <div id='image-card' className='flex w-full h-64 max-h-64 object-contain overflow-hidden rouded-xl'>
+        <Image
+          src={src}
+          alt={alt}
+          width={300}
+          height={300} 
+          className='object-cover min-w-full h-full self-center p-2 overflow-hidden'
+        />
+      </div>
+      <div className='capitalize'>{alt}</div>
     </div>
   );
 }
