a047873c67b96204477e34d7aebde779453d23c5
ziikuu <angelo.bringula@gmail.com>
2025-10-24 14:30:00 +0800

    Refactor Contacts Section: adjust icon sizes and contact information formating

--- components/sections/Contacts.tsx
+++ components/sections/Contacts.tsx
@@ -4,16 +4,16 @@ import { IconContext } from 'react-icons';
 
 const Contacts = () => {
   return (
-    <section id='Contacts' className='space-y-5 h-full min-w-[400px]'>
+    <section id='Contacts' className='space-y-5 h-full'>
         <h1>Contact Us</h1>
         <p>Feel free to contact us if you have any inquiries! You can also reach out through Viber using my number.</p>
         <div className='flex space-x-5 mt-10 items-center font-black'>
-            <IoCallOutline size={50} />
-            <p>09175165398</p>
+            <IoCallOutline size={40} />
+            <small>09175165398</small>
         </div>
         <div className='flex space-x-5 mt-10 items-center font-black'>
-            <IoMailOutline size={50} />
-            <p>bmariacharina@gmail.com</p>
+            <IoMailOutline size={40} />
+            <small>bmariacharina@gmail.com</small>
         </div>
     </section>
   )
